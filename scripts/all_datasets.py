cd8b9d8d559c8bc256264cbcd56a6713dc5dd801
Laura Sheble <sheble@live.unc.edu>
2014-05-02 15:52:41 -0400

    draft update for all_datasets.py to work with newer data set

--- scripts/all_datasets.py
+++ scripts/all_datasets.py
@@ -6,7 +6,13 @@ WoS citations, and GS search results.
 >>> len(data_files)
 11
 '''
+'''
+# ls note: deposit date ideally would have been 2005 for all datasets; when not indicated by repository, 
+date of publication used (as per http://researchremix.wordpress.com/2011/02/16/choosing-repositories-for-the-tracking-data-reuse-project/:
+"In some repositories it is very difficult to determine the date of deposit. 
+I use date of article publication as an imperfect proxy.") cf. Jonathan's notes (https://notebooks.dataone.org/data-reuse/links-to-our-data/) 
 
+'''            
 import csv
 import os
 import fnmatch
@@ -15,14 +21,18 @@ from process_dataset_list import clean_repo_name
 
 year_regex = re.compile('[(\[ ]([1-2][0-9]{3})[)\].]')
 
-data_files = fnmatch.filter(os.listdir('data/repo_datasets/'), 
-                            '*_datasets.csv')
+# ls data_files = fnmatch.filter(os.listdir('data/repo_datasets/'), 
+# ls                          '*_datasets.csv')
+
+data_files = fnmatch.filter(os.listdir('data/cleaner_old_all_datasets/'), 
+                            '*.tsv')
 
 if __name__ == '__main__':
     print 'repo\tid\twos\tgs\tyear'
     
     for data_file in data_files:
-        path = os.path.join('data/repo_datasets', data_file)
+# ls         path = os.path.join('data/repo_datasets', data_file)
+        path = os.path.join('data/cleaner_old_all_datasets', data_file)
         repo = clean_repo_name(data_file[:-len('_datasets.csv')])
         with open(path) as input_file:
             r = csv.reader(input_file)
@@ -43,9 +53,10 @@ if __name__ == '__main__':
                 gs_col = gs_cols[0]
             except IndexError:
                 gs_col = None
-            
             date_cols = [n for n, x in enumerate(header)
-                         if 'date' in x.lower() or 'year' in x.lower()]
+# ls                        if 'date' in x.lower() or 'year' in x.lower()]
+                         if 'date made public' in x.lower()]
+                         
             try:
                 assert len(date_cols) == 1
                 date_col = date_cols[0]
@@ -57,6 +68,8 @@ if __name__ == '__main__':
                     # if there's not a date column, try to parse it out of each 
                     # line with regular expressions
                     date_col = None
+
+
             
             for line in r:
                 if len(line) <= 1: continue
@@ -69,6 +82,8 @@ if __name__ == '__main__':
                     vals.append(wos)
                     
                     if gs_col is None: gs = 0
+# ls: do we want to account for NA's like this?
+                    elif: gs_col == 'NA': gs = 0
                     else: 
                         gs = line[gs_col].split()[0]
                         if not gs.strip(): gs = 0
